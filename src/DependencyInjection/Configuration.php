0d8ad5af693fd62a174c7c0c5761ef3b627a28c7
Nikita Loges <git@loges.one>
2019-01-25 16:13:52 +0100

    fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -19,8 +19,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('shapecode_doctrine_encrypt');
+        $treeBuilder = new TreeBuilder('shapecode_nya_doctrine_encrypt');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('shapecode_nya_doctrine_encrypt');
+        }
 
         $rootNode
             ->children()
